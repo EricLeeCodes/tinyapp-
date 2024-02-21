eb99f705e0ceb30c221970dae8b9a5f947d7e00e
Eric Lee <ericleecodes@gmail.com>
2024-02-20 18:23:58 -0800

    Added username: req.cookies[username to each view

--- express_server.js
+++ express_server.js
@@ -22,17 +22,22 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
+    username: req.cookies["username"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+
   res.render("urls_show", templateVars);
 });
 
